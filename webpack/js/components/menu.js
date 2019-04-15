c9d8173b24ee71333b558ed0da094af0469db891
William Gomez <wgomez@hugeinc.com>
2019-04-15 13:56:28 -0500

    Changes in linter and menu with language.

--- webpack/js/components/menu.js
+++ webpack/js/components/menu.js
@@ -1,49 +1,49 @@
-import throttle from 'lodash/throttle';
+import throttle from 'lodash/throttle'
 
 class Menu {
-  constructor() {
-    this.setDOM();
-    this.setEvents();
+  constructor () {
+    this.setDOM()
+    this.setEvents()
 
     this.states = {
       'OPEN': 'general__menu--open',
       'NOSCROLL': 'body--noscroll',
       'BACKGROUND': 'wrapper-hamburger--scrolled'
-    };
+    }
   }
 
   setDOM () {
-    this.node = document.querySelector('.general__menu');
-    this.button = document.querySelector('.general__hamburger');
+    this.node = document.querySelector('.general__menu')
+    this.button = document.querySelector('.general__hamburger')
     this.wrapperMenu = document.querySelector('.wrapper-hamburger')
-    this.body = document.body;
+    this.body = document.body
   }
 
   setEvents () {
-    this.button.addEventListener('click', this.openMenu.bind(this));
-    document.addEventListener('click', this.closeMenu.bind(this));
-    window.addEventListener('scroll', throttle(this.setMenuBackground.bind(this), 200));
+    this.button.addEventListener('click', this.openMenu.bind(this))
+    document.addEventListener('click', this.closeMenu.bind(this))
+    window.addEventListener('scroll', throttle(this.setMenuBackground.bind(this), 200))
   }
 
   openMenu (event) {
-    this.node.classList.add(this.states['OPEN']);
-    this.body.classList.add(this.states['NOSCROLL']);
+    this.node.classList.add(this.states['OPEN'])
+    this.body.classList.add(this.states['NOSCROLL'])
   }
 
   closeMenu (event) {
     if (event.target.classList.contains('container')) {
-      this.node.classList.remove(this.states['OPEN']);
-      this.body.classList.remove(this.states['NOSCROLL']);
+      this.node.classList.remove(this.states['OPEN'])
+      this.body.classList.remove(this.states['NOSCROLL'])
     }
   }
 
   setMenuBackground () {
     if (window['scrollY'] > 0) {
-      this.wrapperMenu.classList.add(this.states['BACKGROUND']);
+      this.wrapperMenu.classList.add(this.states['BACKGROUND'])
     } else {
-      this.wrapperMenu.classList.remove(this.states['BACKGROUND']);
+      this.wrapperMenu.classList.remove(this.states['BACKGROUND'])
     }
   }
 }
 
-export default Menu;
+export default Menu
--- webpack/js/main.js
+++ webpack/js/main.js
@@ -1,4 +1,4 @@
 import '../scss/main.scss'
 import Menu from './components/menu'
 
-const menu = new Menu();
+const menu = new Menu() // eslint-disable-line
